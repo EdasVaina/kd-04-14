e641ed6e1c5eb9195e399cfaebf53d7e09269a8e
Moksleivis <Moksleivis@kitm.local>
2025-04-14 15:18:26 +0300

    third commit

--- src/components/MinecraftH.jsx
+++ src/components/MinecraftH.jsx
@@ -11,30 +11,38 @@ function MinecraftH() {
     const [x, seInputValue] = useState('');
     const [y, sInputValue] = useState('');
 
-    let sz = Number(z)
-    let sy = Number(y)
-    let sx = Number(x)
-
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
-        alert(sz)
     };
 
     const haInputChange = (event) => {
         seInputValue(event.target.value);
-        alert(sx)
     };
 
     const hInputChange = (event) => {
         sInputValue(event.target.value);
-        alert(sy)
     };
+
+
+    let sz = Number(z)
+    let sy = Number(y)
+    let sx = Number(x)
+
+
+    const Calculator = () => {
+      let siena1 = sx * sy - (2 * sy)
+      let siena2 = sz * sy - (2 * sy)
+      let viskas = siena1 * 2 + siena2 * 2 + sy * 4 + sz * sx
+      alert(viskas + " blocks")
+    };
+
 
   return (
     <div>
        <input type="text" ref={inputRef} value={z} onChange={handleInputChange} placeholder="Plotis" ></input>
       <input type="text" ref={inpRef} value={x} onChange={haInputChange} placeholder="Ilgis" ></input>
       <input type="text" ref={inRef} value={y} onChange={hInputChange} placeholder="Aukstis" ></input>
+      <button type="submit" onClick={Calculator}>caculate</button>
     </div>
     
   )
